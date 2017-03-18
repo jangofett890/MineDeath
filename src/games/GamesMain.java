6f90d5565e27324713208df0de7678c23339a3ab
ConfinedFish <alexzeigler0317@gmail.com>
2017-03-17 18:37:17 -0700

    Started work on flags

--- src/games/GamesMain.java
+++ src/games/GamesMain.java
@@ -1,5 +1,21 @@
 package games;
 
-public class GamesMain {
+import java.util.HashMap;
 
+import flags.Bool;
+import flags.Flag;
+
+public class GamesMain {
+	public HashMap<String, Flag<?>> flags = new HashMap<String, Flag<?>>();
+	private Bool teams = new Bool("teams", false);
+	
+	public void setupFlags(){
+		addFlag(teams);
+	}
+	private void addFlag(Flag<?> flag){
+		flags.put(flag.getName(), flag);
+	}
+	public Flag<?> getFlag(String name){
+		return flags.get(name);
+	}
 }
